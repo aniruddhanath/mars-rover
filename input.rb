f326ffec0f50741f00f029df53e7c9c7c8a1d86c
Aniruddha Nath <aniruddhanath1@gmail.com>
2016-04-18 21:14:41 +0530

    removes </br>

--- input.rb
+++ input.rb
@@ -32,18 +32,14 @@ class Input
 
   def parse_input(user_input)
     input_array = user_input.split("\n")
-    input_array.shift # check for starting blank line
 
-    # first line
     input = input_array.shift
-    line_array = input.split("</br>")
-    grid_limits = line_array[0].split(" ")
+    grid_limits = input.split(" ")
     @grid = { x: grid_limits[0].to_i, y: grid_limits[1].to_i }
     
-    input_array.each_with_index do |input, index|
-      next if (input == "END")
-      line_array = input.split("</br>")
-      line = line_array[0]
+    input_array.each_with_index do |line, index|
+      line = line.strip
+      next if line.nil? || line == ""
       index % 2 == 0 ? store_position(line) : store_path(line)
     end
   end
